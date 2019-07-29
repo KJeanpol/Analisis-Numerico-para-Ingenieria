7a8fd967cd9ab3e8a786408a2365b27735cd3306
Kenneth Jeanpol Alvarado Mendez <ce.kalvarado@gmail.com>
2019-07-29 10:22:38 -0600

    Integral approach through Taylor Series functions

--- Series de Taylor/SerieTaylor.py
+++ Series de Taylor/SerieTaylor.py
@@ -1,2 +1,36 @@
-def serieTaylero():
+def serieTaylor(error):
+    m= cantTerminos(error)
+    aprox=valorIntegral(m)
+    print ("Numero de terminos de la serie que aproxima la integral: " + str(m) +"\n")
+    print ("Aproximacion de la Integral: "+ str(aprox))
     return 0
+
+def cantTerminos(tol):
+    m=0
+    tolAux=tolerancia(m)
+    while (tolAux>=tol):
+        m+=1
+        tolAux=tolerancia(m)
+    return m
+
+def tolerancia (m):
+    numerador=pow(2,(m+1))
+    denominador=factorial(m+1)*(2*(m+1)+1)
+    tolAux=numerador/denominador
+    return tolAux
+def factorial (num):
+    result=1
+    while (num>1):
+        result*=num
+        num-=1
+    return result
+    
+def valorIntegral(n):
+    sumatoria=0
+    resultado=0
+    while(sumatoria<=n):  
+        numerador= (pow(-1,n))*(pow(2,n))
+        denominador= factorial(n)*(2*n+1)   
+        resultado+= numerador/denominador
+        n-=1
+    return resultado
