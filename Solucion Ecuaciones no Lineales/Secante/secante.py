7606fcc18d3322acc13251efd99de6efd989c8f1
Kenneth Jeanpol Alvarado Mendez <ce.kalvarado@gmail.com>
2019-08-11 21:51:09 -0600

    Secant method implemented on python, missing graph

--- Solucion Ecuaciones no Lineales/Secante/secante.py
+++ Solucion Ecuaciones no Lineales/Secante/secante.py
@@ -1,15 +1,45 @@
 # -*- coding: utf-8 -*-
-"""
-Editor de Spyder
-
-Este es un archivo temporal.
-"""
-
 import numpy as np
 import matplotlib.pyplot as plt
+import sympy
 
-def h(x):
-    return np.sin(x)
+def evaluar(funcion,varx):
+    x,y,z = sympy.symbols ('x y z')
+    y=sympy.sympify(funcion).subs(x,varx)
+    #print (float(y))
+    return float(y)
 
-x=np.linspace(0,10,100)
-plt.plot(x,h(x))
+def graficar(funcion,x0,x1,tol):
+    x = range(x1, secante(funcion,x0,x1,tol))
+    print(x)
+    itera=range(0,secante(funcion,x0,x1,tol))
+    plt.plot([evaluar(funcion,i) for i in itera],x,'ro')
+    plt.plot([evaluar(funcion,i) for i in itera],x,label='Metodo de Secante')
+    plt.legend()
+    plt.xlabel('Iteraciones')
+    plt.ylabel('| f(X) |')
+    plt.show()
+
+def error(x1,x):
+    return (x1-x)/x1
+
+def secante(funcion,xk0,xkn,tol):
+    xk=calSecante(funcion,xk0,xkn)
+    xk1=calSecante(funcion,xkn,xk)
+    k=2
+    while (error(xk1,xk)>=tol):  
+        xk=calSecante(funcion,xk,xk1)
+        xk1=calSecante(funcion,xk1,xk) 
+        k+=2
+    #print("xk1 es: " + str(xk1))
+    #print(k)
+    return k
+
+def calSecante(funcion,xk,xk1):
+    primera=evaluar(funcion,xk)
+    segunda=evaluar(funcion,xk1)
+    fraccion=(xk-xk1)/(primera-segunda)
+    return float(xk-(fraccion)*(evaluar(funcion,xk)))
+
+
+    
