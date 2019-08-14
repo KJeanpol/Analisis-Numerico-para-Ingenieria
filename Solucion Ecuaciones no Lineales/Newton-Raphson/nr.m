9280271cf5d49e4a069a3eb366ed7c4f156728de
Kenneth Jeanpol Alvarado Mendez <ce.kalvarado@gmail.com>
2019-08-14 17:08:44 -0600

    Optimized code

--- Solucion Ecuaciones no Lineales/Newton-Raphson/nr.m
+++ Solucion Ecuaciones no Lineales/Newton-Raphson/nr.m
@@ -8,29 +8,17 @@ function resultado= nr(func,w,tol)
         xk1=calNewtonRaph(funcion,xk);
         k+=2;
     endwhile
-    resultado=k
+    resultado=xk1;
 endfunction
-
-function resultado = evaluar(funcion,varx)
-    resultado= funcion(varx);
-endfunction   
- 
- function derivada= calDerivada(funcion)
-    syms x;  
-    derivada=diff(funcion,x);
-endfunction 
 
 function err=error(x1,x)
     err= (x1-x)/x1;
 endfunction 
 
-
 function newton=calNewtonRaph(funcion,w)
     syms x;
     f=inline(funcion);
-    class(f)
     derivada=char(diff(funcion,x));
-    class(derivada)
     df=inline(derivada);
-    newton=w-(f(w)/df(w))
+    newton=w-(f(w)/df(w));
 endfunction 
